b4e5522521e546d443a868c9d37cbb01c8fe6ffc
chanho <hpyho33@kookmin.ac.kr>
2018-07-27 20:59:58 +0900

    same

--- phptest.php
+++ phptest.php
@@ -9,33 +9,25 @@
 </body>
 </html>
 <?php
-$id = $_REQUEST['Id'];
-$password =$_REQUEST['Passwd'];
-$prevpage = $_SERVER['HTTP_REFERER'];
-if(strlen($id)<=0){
-echo "<script>alert(\"push your id or password\");</script>";
-header('location:'.$prevpage);
-}
-
 $db_host = "localhost";
 $db_user="root";
 $db_passwd = "kjs751300aa";
 $db_name = "user_account";
 $conn = mysqli_connect($db_host,$db_user,$db_passwd,$db_name);
-echo "id = ";
 if (!$conn){
-die('could not connect'.mysqli_connect_errno());
+	die('could not connect'.mysqli_connect_errno());
 }
 else{
-$sql = "INSERT INTO topic(id, password) VALUES('$id', '$password');";
-$result = mysqli_query($conn, $sql);
-if ($result === false){
-    echo mysqli_error($conn);
-}
-else {
-$row = mysql_fetch_row('SELECT id FROM topic');
-echo $row;
-}
-}
+	$select_query = "SELECT id, password FROM newuser";
+	$result = mysqli_query($conn, $select_query);
+
+	$row = mysqli_fetch_array($result);
+	for($count = 0;3<=$count; $count++){
+		print_r($row[$count]);
+	}
+	
+
+	}
+
 
 ?>
